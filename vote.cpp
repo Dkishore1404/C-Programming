1187440a31bc844ed50422bc03b5c27eded0a937
Dkishore1404 <123447168+Dkishore1404@users.noreply.github.com>
2023-01-24 14:03:00 +0530

    Add files via upload

---
+++ vote.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("enter your age");
+	scanf("%d",&a,&b);
+	if(a>=18)
+	printf("you are eligible for voting");
+	else
+	printf("you are not elibible for voting");
+	
+	
+}
